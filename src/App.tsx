3ebdd9a91f76911e3c96e121d401a9fe8c3e8b02
Volkan Sönmez <hvsonmez@gmail.com>
2023-12-24 12:39:23 +0300

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
 
   useEffect(() => {
     addMessage("Welcome message", "success");
+    addMessage("Welcome warning", "warning");
   }, []);
 
   return (
--- src/locations/components/CityDetail.component.tsx
+++ src/locations/components/CityDetail.component.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useState } from "react";
-import ButtonComponent from "../../shared-components/Button.component";
+import React from "react";
 import IconInfoComponent from "../../assets/images/svg-icons/IconInfo.component";
 import Location from "../models/location.type";
 import { useTranslation } from "react-i18next";
@@ -8,22 +7,19 @@ import useCharacterLocation from "../../store/hooks/character/use-character-loca
 const CityDetail = ({ city }: { city: Location }) => {
   const { i18n, t } = useTranslation();
   const { characterLocation } = useCharacterLocation();
-  const [isShowDetail, setIsShowDetail] = useState<boolean>(false);
-  const toggleShowDetail = useCallback(() => {
-    setIsShowDetail(!isShowDetail);
-  }, [isShowDetail, setIsShowDetail]);
+
   return (
     <>
-      <div className="flex items-center">
-        <span className="block">{city.name}</span>
-        <ButtonComponent className="ml-auto border-0" onClick={toggleShowDetail}>
+      <details>
+        <summary className="flex items-baseline gap-2">
           <IconInfoComponent></IconInfoComponent>
-        </ButtonComponent>
-      </div>
-      {isShowDetail && <small>{i18n.language === "en" ? city.description.en : city.description.tr}</small>}
-      {characterLocation !== city.id && isShowDetail && (
-        <small className="block my-1 text-orange-300">{t("Travel Info")}</small>
-      )}
+          <span>{city.name}</span>
+        </summary>
+        <div>
+          <small>{i18n.language === "en" ? city.description.en : city.description.tr}</small>
+          {characterLocation !== city.id && <small className="block my-1 text-orange-300">{t("Travel Info")}</small>}
+        </div>
+      </details>
     </>
   );
 };
